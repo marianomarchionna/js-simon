5784ebf4eba1ff77044f29d1efa428ba0c526418
Mariano <marianomarchionna8@gmail.com>
2021-07-29 16:03:56 +0200

    Stampa numeri uguali e contatore

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // Un alert() espone 5 numeri generati casualmente.
 let numeri = [];
-var numeriUtente = [];
+let numeriUtente = [];
+let cont = 0;
 
 while (numeri.length < 5) {
     let numeroGenerato = randomNumber(1, 100);
@@ -11,13 +12,12 @@ while (numeri.length < 5) {
 
 alert('I numeri da memorizzare sono ' + numeri);
 
+
 // Da li parte un timer di 30 secondi.
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-alert('Inserisci i numeri che ricordi (non puoi inserire due numeri uguali)')
-setTimeout(inserisciNumeri, 2000);
-
-
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+alert('Inserisci i numeri che ricordi (non puoi inserire due numeri uguali)');
+setTimeout(inserisciNumeri, 2000);
 
 
 //funzioni
@@ -32,4 +32,13 @@ function inserisciNumeri() {
             numeriUtente.push(numeroInserito);
         }
     }
+    console.log('I numeri che ti sei ricordato sono: ');
+    for(let i = 0; i < 5; i++){
+        if(numeri.includes(numeriUtente[i])){
+            console.log(numeriUtente[i]);
+            cont++;
+        }
+    }
+    console.log('(' + cont + ')');
+    console.log('I numeri che dovevi memorizzare memorizzare erano ' + numeri);
 }
